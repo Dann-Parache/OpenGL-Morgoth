70a096ffca54100ac380a7f53802d8afd5d321b5
Taylor Andrews <taan7753@colorado.edu>
2015-12-05 19:21:34 -0700

    Updated shadows

--- scene.c
+++ scene.c
@@ -4,16 +4,10 @@
  */
 
 #include "scene.h"
- 
-/* Global Variables */
-extern unsigned int textures[10]; // In case we need textures
 
 /* Draw the Scene */
-void drawScene(int light, double t) {
-    //t = 0; // Uncomment to stop animation (for debugging)
-
-    /* Setup Lights */
-    Lights(light, t);
+void drawScene(double t) {
+    // t = 0; // Uncomment to stop animation (for debugging)
 
     /* Draw the Larger Fighter */
     glPushMatrix();
@@ -30,26 +24,19 @@ void drawScene(int light, double t) {
     glScaled(.6, .6, .6);
         drawFin(0, 3, 0, 7.5, 7.5, 7.5, t);
     glPopMatrix();
-
-    if (!light) 
-        return; // Floor has no shadow, exit early
-
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, ground[0]);
-
-    glColor3f(.38, .20, .07); // Medium Brown
-
-    /* Surrounding Arena */
-    drawSquare();
-
-    glDisable(GL_TEXTURE_2D);
 }
 
-/* Helper Function to Draw a Simple Square */
-void drawSquare() {
+/* Function to draw a the ground */
+void drawSquares() {
     int i, j; // Loop Iterators
     int n = 20; // Number of chunks in the big square
     double s = 10; // Size of the square
+
+    glColor3f(.38, .20, .07); // Medium Brown
+
+    /* Enable Textures */
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, ground[0]);
 
     glNormal3f(0, 1, 0);
     for (j =- n; j < n; j++)
@@ -62,47 +49,6 @@ void drawSquare() {
         }
         glEnd();
    }
-}
 
-/*
- * Create the Lights
- * Adapted from Example 36
- */
-void Lights(int light, double t)
-{
-    // Set light position
-    Lpos[0] = 4*Cos(t);
-    Lpos[1] = ylight;
-    Lpos[2] = 4*Sin(t);
-    Lpos[3] = 1;
-
-    // Enable Lighting
-    if (light)
-    {
-        float Med[]  = {0.3, 0.3, 0.3, 1.0};
-        float High[] = {1.0, 1.0, 1.0, 1.0};
-
-        // Enable lighting with normalization
-        glEnable(GL_LIGHTING);
-        glEnable(GL_NORMALIZE);
-
-        // glColor sets ambient and diffuse color materials
-        glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
-        glEnable(GL_COLOR_MATERIAL);
-
-        // Light 0
-        glEnable(GL_LIGHT0);
-        glLightfv(GL_LIGHT0,GL_POSITION,Lpos);
-        glLightfv(GL_LIGHT0,GL_AMBIENT,Med);
-        glLightfv(GL_LIGHT0,GL_DIFFUSE,High);
-
-        // Light 1
-    }
-    // Disable Lighting
-    else
-    {
-        glDisable(GL_LIGHTING);
-        glDisable(GL_COLOR_MATERIAL);
-        glDisable(GL_NORMALIZE);
-    }
+    glDisable(GL_TEXTURE_2D); // Disable textures
 }
--- scene.h
+++ scene.h
@@ -6,23 +6,8 @@
 #ifndef SCENE_H
 #define SCENE_H
  
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <math.h>
-
-#define GL_GLEXT_PROTOTYPES // OpenGL with prototypes for glext
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
-
-#include "CSCIx229.h"
 #include "mor.h"
 #include "fin.h"
-
-#define PI 3.141592
 
 /* Vector */
 typedef struct {
@@ -35,20 +20,8 @@ typedef struct {
 extern unsigned int textures[10];
 extern unsigned int ground[1];
 
-extern float Lpos[4];
-extern float ylight;
-
 /* Draw the Scene */
-void drawScene(int light, double t);
-void drawSquare();
-void Lights(int light, double t); // Draw Light
-
-/* Glut Functions */
-void project();
-void display();
-void key(unsigned char ch, int x, int y);
-void special(int key, int x, int y);
-void idle();
-void reshape(int width, int height);
+void drawScene(double t);
+void drawSquares(); // Draw the ground
 
 #endif
