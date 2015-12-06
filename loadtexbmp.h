f590295804ee1582c4c41565b4a230bc380b236e
Taylor Andrews <taan7753@colorado.edu>
2015-12-05 19:20:49 -0700

    Created header file for loadtexbmp

---
+++ loadtexbmp.h
@@ -0,0 +1,29 @@
+/* 
+ * File to load a bmp as a texture in opengl.
+ *
+ * Created by: Willem A. (Vlakkies) Schreüder 
+ * Modified by: Taylor Andrews
+ */
+
+#ifndef LOADTEXBMP
+#define LOADTEXBMP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+
+#ifdef USEGLEW
+#include <GL/glew.h>
+#endif
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+unsigned int LoadTexBMP(const char* file);
+
+#endif
